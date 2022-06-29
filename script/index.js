816bac6a00069286706ad106571472005bd9dc3d
AlexDom88 <91887790+AlexDom88@users.noreply.github.com>
2022-06-29 18:50:16 +0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,21 +1,21 @@
 "use strict";
 //  Обращение к пользователю
 
-    let nameUser=prompt("Как тебя зовут?");
+    let nameUser = prompt("Как тебя зовут?");
     console.log (nameUser);
-    let surnameUser=prompt("Какая у тебя фамилия?");
+    let surnameUser = prompt("Какая у тебя фамилия?");
     console.log (surnameUser);
-    let ageUser=prompt ("Какой у тебя возраст?");
+    let ageUser = prompt("Какой у тебя возраст?");
     console.log (ageUser);
 
     // Среднее арифметическое
     
-    let a=prompt("Введите первое число");
-    a=parseInt(a);
-    let b=prompt("Введите второе число");
-    b=parseInt(b);
-    let c=prompt("Введите третье число");
-    c=parseInt(c);
-    let result=(a+b+c)/3;
+    let a = prompt("Введите первое число");
+    a = parseInt(a);
+    let b = prompt("Введите второе число");
+    b = parseInt(b);
+    let c = prompt("Введите третье число");
+    c = parseInt(c);
+    let result = (a+b+c)/3;
     console.log(result);
 
